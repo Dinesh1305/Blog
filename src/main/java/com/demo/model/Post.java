e57e9cc294790e934f27af2febd7e0f4c85e39ec
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-07 21:00:02 +0530

    The Post Entity was created

---
+++ src/main/java/com/demo/model/Post.java
@@ -0,0 +1,74 @@
+package com.demo.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.UUID)
+	private UUID id;
+	
+	
+	@Column(nullable=false)
+	private String content;
+	
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private PostType type;
+	
+	
+	@Column(nullable=false)
+	private Integer readingTime;
+	
+	@CreationTimestamp
+	@Column(nullable=false)
+	private LocalDateTime createat;
+	
+	@Column(nullable=false)
+	private LocalDateTime updatedAt;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(content, createat, id, readingTime, type, updatedAt);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(content, other.content) && Objects.equals(createat, other.createat)
+				&& Objects.equals(id, other.id) && Objects.equals(readingTime, other.readingTime) && type == other.type
+				&& Objects.equals(updatedAt, other.updatedAt);
+	}
+	
+	
+	
+	
+}
