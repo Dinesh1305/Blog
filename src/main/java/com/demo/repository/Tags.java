d0b54d85ef86ee790c084f3c9fdd4a53b155b1a3
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-09 22:36:24 +0530

    The @Repository was added for all the repo interface

--- src/main/java/com/demo/repository/Tags.java
+++ src/main/java/com/demo/repository/Tags.java
@@ -3,7 +3,8 @@ package com.demo.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface Tags extends JpaRepository<Tags,UUID> {
 
 }
