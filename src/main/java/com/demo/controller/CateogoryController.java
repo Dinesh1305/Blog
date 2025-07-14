17a47b8b75372157330d741366c120275752261e
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-14 22:54:01 +0530

    The CateogoryService class was created

--- src/main/java/com/demo/controller/CateogoryController.java
+++ src/main/java/com/demo/controller/CateogoryController.java
@@ -1,5 +1,19 @@
 package com.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor	
 public class CateogoryController {
 
+	
+	
+	@GetMapping()
+	public ResponseEntity<List<CateogoryDto>> listofCategory(){
+		
+	}
 }
--- src/main/java/com/demo/model/dto/CategoryDto.java
+++ src/main/java/com/demo/model/dto/CategoryDto.java
@@ -1,5 +1,19 @@
 package com.demo.model.dto;
 
-public class CategoryDto {
+import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class CategoryDto {
+private UUID id;
+private String name;
+private long postcount;
 }
